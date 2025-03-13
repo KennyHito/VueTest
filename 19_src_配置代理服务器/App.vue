702144929b28a715b39b06db9fc442ad8325be0e
yuhaitao <yuhaitao1@lczq.com>
2025-03-13 11:06:44 +0800

    modify: 增加本地写的服务器代码zip,解压后使用node server1的方式启动服务

--- 19_src_配置代理服务器/App.vue
+++ 19_src_配置代理服务器/App.vue
@@ -6,30 +6,32 @@
 </template>
 
 <script>
-	import axios from 'axios'
-	export default {
-		name:'App',
-		methods: {
-			getStudents(){
-				axios.get('http://localhost:8080/students').then(
-					response => {
-						console.log('请求成功了',response.data)
-					},
-					error => {
-						console.log('请求失败了',error.message)
-					}
-				)
-			},
-			getCars(){
-				axios.get('http://localhost:8080/demo/cars').then(
-					response => {
-						console.log('请求成功了',response.data)
-					},
-					error => {
-						console.log('请求失败了',error.message)
-					}
-				)
-			}
+import axios from 'axios'
+export default {
+	name: 'App',
+	methods: {
+		getStudents() {
+			// 这里的地址根据自己本地服务器地址进行修改
+			axios.get('http://localhost:8080/students').then(
+				response => {
+					console.log('请求成功了', response.data)
+				},
+				error => {
+					console.log('请求失败了', error.message)
+				}
+			)
 		},
-	}
+		getCars() {
+			// 这里的地址根据自己本地服务器地址进行修改
+			axios.get('http://localhost:8080/demo/cars').then(
+				response => {
+					console.log('请求成功了', response.data)
+				},
+				error => {
+					console.log('请求失败了', error.message)
+				}
+			)
+		}
+	},
+}
 </script>
